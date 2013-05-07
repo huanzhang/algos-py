d6105d42c8bd0f8b5f89095aa16ab615a6ddd209
Huan Zhang <hzhang@redhat.com>
2013-05-07 15:20:34 +0800

    Change bubble_sort

--- algos/sorting/bubble_sort.py
+++ algos/sorting/bubble_sort.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 def sort(L):
-    if len(L) <= 1:
-        return L
-    for i in range(1, len(L)):
-        for j in range(1, len(L)):
+    for i in range(0, len(L)):
+        for j in range(1, len(L)-i):
             if L[j] < L[j-1]:
                 L[j-1], L[j] = L[j], L[j-1]
 
