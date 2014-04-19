da67fc9854565fa76457b2a26a89efef1862d2a8
Huan Zhang <hzhang@redhat.com>
2014-04-20 06:26:17 +0800

    Using Graph

--- ds/decipher.py
+++ ds/decipher.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python2.7
 
 import itertools
+import re
+import sys
+
 
 class DiGraph:
     """
     Class for directed graphs.
     """
     def __init__(self):
-        self.node = {} # dictionary for node attributes
-        self.adj = {}  # empty adjacency dict
+        self.node = {}  # dictionary for node attributes
+        self.adj = {}   # empty adjacency dict
 
     def __iter__(self):
         """
@@ -16,7 +19,7 @@ class DiGraph:
         """
         return iter(self.node)
 
-    def __contains__(self,n):
+    def __contains__(self, n):
         """
         Return True if n is a node, False otherwise. Use the expression
         'n in G'.
@@ -41,12 +44,12 @@ class DiGraph:
         """
         # set up attribute dict
         if attr_dict is None:
-            attr_dict=attr
+            attr_dict = attr
         else:
             try:
                 attr_dict.update(attr)
             except AttributeError:
-                raise Exception(\
+                raise Exception(
                     "The attr_dict argument must be a dictionary.")
         if n not in self.adj:
             self.adj[n] = {}
@@ -59,29 +62,27 @@ class DiGraph:
         Add an edge between u and v.
         """
         # add the edge
-        self.adj[u][v]=self.adj[u].get(v,{})
+        self.adj[u][v] = self.adj[u].get(v, {})
 
 
 #######################################################
 #################### Main #############################
 #######################################################
 
-if __name__ == "__main__":
-    pass
-
 def decipher(word_set, text):
-    """ 
+    """
     decipher(word_dict, text) --> deciphered text
 
-    return the deciphered text of encrypted text 
+    return the deciphered text of encrypted text
     """
     word_dict = {}
     for x in word_set:
-        if word_dict.has_key(len(x)):
+        if len(x) in word_dict:
             word_dict.get(len(x)).append(x)
         else:
             word_dict[len(x)] = [x]
 
+    # start to build the graph
     text_words = text.split()
     G = DiGraph()
     start_nodes = []
@@ -96,51 +97,72 @@ def decipher(word_set, text):
             G.add_node(i, word=w)
             curr_nodes.append(i)
             for j in end_nodes:
-                G.add_edge(j,i)
+                G.add_edge(j, i)
         if not end_nodes:
             start_nodes = curr_nodes
         end_nodes = curr_nodes
 
-    for x,y in [(x,y) for x in start_nodes for y in end_nodes]:
-        p = find_all_paths(G,x,y,text)
+    deciphered_text = []
+    # find paths
+    for x, y in [(x, y) for x in start_nodes for y in end_nodes]:
+        p = find_paths(G, x, y, text)
         if p:
             for x in p:
+                t = []
                 for y in x:
-                    print G.node[y].get("word"),
-                print ""
+                    t.append(G.node[y].get("word"))
+                if t:
+                    deciphered_text.append(" ".join(t))
+    if deciphered_text:
+        return deciphered_text[0]
+    else:
+        return re.sub(r'\S', '*', text)
 
 
-
-def find_all_paths(graph, start, end, text, path=[]):
+def find_paths(graph, start, end, text, path=[]):
+    """ Find paths from start to end"""
     path = path + [start]
     if start == end:
         return [path]
     if start not in graph:
         return []
     paths = []
-    if is_conflicted(graph,path,text):
+    if is_conflicted(graph, path, text):
         return []
     for node in graph[start]:
         if node not in path:
-            newpaths = find_all_paths(graph, node, end, text, path)
+            newpaths = find_paths(graph, node, end, text, path)
             for newpath in newpaths:
-                if not is_conflicted(graph,newpath,text):
+                if not is_conflicted(graph, newpath, text):
                     paths.append(newpath)
     return paths
+
 
 def is_conflicted(graph, path, text):
     test_str = []
     for i in path:
         test_str.append(graph.node[i].get("word"))
-    if test_str and _is_conflicted(text.replace(" ",""),"".join(test_str)):
+    if test_str and _is_conflicted(text.replace(" ", ""), "".join(test_str)):
         return True
-    
+
+
 def _is_conflicted(str1, str2):
     m = {}
-    for x,y in zip(str1, str2):
-        if m.has_key(x):
-            if m[x] != y: return True
+    for x, y in zip(str1, str2):
+        if x in m:
+            if m[x] != y:
+                return True
         else:
             m[x] = y
-    return False 
+    return False
 
+if __name__ == "__main__":
+    lines = sys.stdin.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip().replace('\n', '').replace('\r', '')
+    n = int(lines[0])
+    word_set = set([])
+    for x in range(1, n+1):
+        word_set.add(lines[x])
+    for y in range(n+1, len(lines)):
+        print decipher(word_set, lines[y])
